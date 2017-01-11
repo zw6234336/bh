f8d95c56df66fa58ecebf3a2e8271b19ce4e0369
zw6234336 <wei6234336@gmail.com>
2017-01-11 17:18:39 +0800

    zhang

---
+++ src/main/java/com/zhang/service/MailService.java
@@ -0,0 +1,31 @@
+package com.zhang.service;
+
+import java.util.List;
+
+import javax.mail.MessagingException;
+
+public interface MailService {
+
+	
+	/**
+	 * 单发
+	 * 
+	 * @param recipient 收件人
+	 * @param subject 主题
+	 * @param content 内容
+	 */
+	public void send(String recipient, String subject, String content);
+	
+	
+	public void sendJava(String recipient, String subject, String content)throws MessagingException;
+	
+	
+	/**
+	 * 群发
+	 * 
+	 * @param recipients
+	 * @param subject
+	 * @param content
+	 */
+	public void send(List<String> recipients, String subject, String content);
+}
