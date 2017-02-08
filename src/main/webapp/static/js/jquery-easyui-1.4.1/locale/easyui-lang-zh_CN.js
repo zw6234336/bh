36885fa38e3ca176bb1fb3db05c6feec3400c97b
zw6234336 <wei6234336@gmail.com>
2017-02-08 08:47:07 +0800

    zhang

---
+++ src/main/webapp/static/js/jquery-easyui-1.4.1/locale/easyui-lang-zh_CN.js
@@ -0,0 +1,80 @@
+if ($.fn.pagination){
+	$.fn.pagination.defaults.beforePageText = '第';
+	$.fn.pagination.defaults.afterPageText = '共{pages}页';
+	$.fn.pagination.defaults.displayMsg = '显示{from}到{to},共{total}记录';
+}
+if ($.fn.datagrid){
+	$.fn.datagrid.defaults.loadMsg = '正在处理，请稍待。。。';
+}
+if ($.fn.treegrid && $.fn.datagrid){
+	$.fn.treegrid.defaults.loadMsg = $.fn.datagrid.defaults.loadMsg;
+}
+if ($.messager){
+	$.messager.defaults.ok = '确定';
+	$.messager.defaults.cancel = '取消';
+}
+$.map(['validatebox','textbox','filebox','searchbox',
+		'combo','combobox','combogrid','combotree',
+		'datebox','datetimebox','numberbox',
+		'spinner','numberspinner','timespinner','datetimespinner'], function(plugin){
+	if ($.fn[plugin]){
+		$.fn[plugin].defaults.missingMessage = '该输入项为必输项';
+	}
+});
+if ($.fn.validatebox){
+	$.fn.validatebox.defaults.rules.email.message = '请输入有效的电子邮件地址';
+	$.fn.validatebox.defaults.rules.url.message = '请输入有效的URL地址';
+	$.fn.validatebox.defaults.rules.length.message = '输入内容长度必须介于{0}和{1}之间';
+	$.fn.validatebox.defaults.rules.remote.message = '请修正该字段';
+}
+if ($.fn.calendar){
+	$.fn.calendar.defaults.weeks = ['日','一','二','三','四','五','六'];
+	$.fn.calendar.defaults.months = ['一月','二月','三月','四月','五月','六月','七月','八月','九月','十月','十一月','十二月'];
+}
+if ($.fn.datebox){
+	$.fn.datebox.defaults.currentText = '今天';
+	$.fn.datebox.defaults.closeText = '关闭';
+	$.fn.datebox.defaults.okText = '确定';
+	$.fn.datebox.defaults.formatter = function(date){
+		var y = date.getFullYear();
+		var m = date.getMonth()+1;
+		var d = date.getDate();
+		return y+'-'+(m<10?('0'+m):m)+'-'+(d<10?('0'+d):d);
+	};
+	$.fn.datebox.defaults.parser = function(s){
+		if (!s) return new Date();
+		var ss = s.split('-');
+		var y = parseInt(ss[0],10);
+		var m = parseInt(ss[1],10);
+		var d = parseInt(ss[2],10);
+		if (!isNaN(y) && !isNaN(m) && !isNaN(d)){
+			return new Date(y,m-1,d);
+		} else {
+			return new Date();
+		}
+	};
+}
+if ($.fn.datetimebox && $.fn.datebox){
+	$.extend($.fn.datetimebox.defaults,{
+		currentText: $.fn.datebox.defaults.currentText,
+		closeText: $.fn.datebox.defaults.closeText,
+		okText: $.fn.datebox.defaults.okText
+	});
+}
+if ($.fn.datetimespinner){
+	$.fn.datetimespinner.defaults.selections = [[0,4],[5,7],[8,10],[11,13],[14,16],[17,19]]
+}
+var $lang={
+		errAlertMsg: "\u4E0D\u5408\u6CD5\u7684\u65E5\u671F\u683C\u5F0F\u6216\u8005\u65E5\u671F\u8D85\u51FA\u9650\u5B9A\u8303\u56F4,\u9700\u8981\u64A4\u9500\u5417?",
+		aWeekStr: ["\u5468","\u65E5","\u4E00","\u4E8C","\u4E09","\u56DB","\u4E94","\u516D"],
+		aLongWeekStr:["\u5468","\u661F\u671F\u65E5","\u661F\u671F\u4E00","\u661F\u671F\u4E8C","\u661F\u671F\u4E09","\u661F\u671F\u56DB","\u661F\u671F\u4E94","\u661F\u671F\u516D"],
+		aMonStr: ["\u4E00\u6708","\u4E8C\u6708","\u4E09\u6708","\u56DB\u6708","\u4E94\u6708","\u516D\u6708","\u4E03\u6708","\u516B\u6708","\u4E5D\u6708","\u5341\u6708","\u5341\u4E00","\u5341\u4E8C"],
+		aLongMonStr: ["\u4E00\u6708","\u4E8C\u6708","\u4E09\u6708","\u56DB\u6708","\u4E94\u6708","\u516D\u6708","\u4E03\u6708","\u516B\u6708","\u4E5D\u6708","\u5341\u6708","\u5341\u4E00\u6708","\u5341\u4E8C\u6708"],
+		clearStr: "\u6E05\u7A7A",
+		todayStr: "\u4ECA\u5929",
+		okStr: "\u786E\u5B9A",
+		updateStr: "\u786E\u5B9A",
+		timeStr: "\u65F6\u95F4",
+		quickStr: "\u5FEB\u901F\u9009\u62E9", 
+		err_1: '\u6700\u5C0F\u65E5\u671F\u4E0D\u80FD\u5927\u4E8E\u6700\u5927\u65E5\u671F\!'
+		} 
